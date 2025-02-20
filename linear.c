1d70b77babd8305190e85d3c7bc199a9dadfebe1
HemalathaDuppy <24wh1a05v2@bvrithyderabad.edu.in>
2025-02-20 15:37:12 +0530

    Add files via upload

---
+++ linear.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+ int i,n,a[100],key,flag;
+ printf("Enter the no.of elements of array:");
+ scanf("%d",&n);
+ printf("Enter the elements into the array:");
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&a[i]);
+  printf("Enter the key elements of the array:");
+  scanf("%d",&key);
+ }
+  for(i=0;i<n;i++)
+  {
+  if(key==a[i])
+  {
+   flag=1;
+   printf("Key is found at %d=i+1",i+1);
+   break;
+  }
+  }
+  if(flag!=0)
+  {
+   printf("Key is not found");
+  }
+return 0;
+}
+
+   
