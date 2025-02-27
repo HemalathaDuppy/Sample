1c9297fbe28c821cc636a4891293e357e3c5561c
HemalathaDuppy <24wh1a05v2@bvrithyderabad.edu.in>
2025-02-27 15:37:09 +0530

    Delete Samplefile.py

--- Samplefile.py
+++
@@ -1,9 +0,0 @@
-no_of_words = int(input())
-
-for _ in range(no_of_words):
-    word = input().strip()
-    if len(word) > 10:
-        print(f"{word[0]}{len(word) - 2}{word[-1]}")
-    else:
-        print(word)
-        print(word)
